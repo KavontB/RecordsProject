5723cbcafb9d774bf7045296ea0de6070343729a
Key343 <keyonna.sharde@gmail.com>
2022-10-23 05:51:32 -0400

    Record Proj

---
+++ MyRecordProject/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace MyRecordProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\kavon\Downloads\SampleFile.txt";
+
+            var file = File.ReadAllLines(path);
+            Validate validate = new Validate();
+            //int counter = 0;
+            List<string> recordNumbers = new List<string>();
+            List<string> accountNumbers = new List<string>();
+            List<string> fName = new List<string>();
+            List<string> amountList = new List<string>();
+            try
+            {
+
+                foreach (var line in file)
+                {
+
+
+                    var spaced = Regex.Replace(line, @"\s+", " ");
+                    var strArr = spaced.Split(' ');
+                    var recAndAcc = strArr[0] + " " + strArr[1];
+                    var record = string.Join("", line.Take(4));
+                    recordNumbers.Add(record);
+
+                    // List<char> rNum = recordNum.ToList();
+                    //var recordNum = line.Take(4);
+                    // var record = string.Join("", recordNum);
+                    //rNums.Add(record);
+
+                    // List<char> rNum = recordNum.ToList();
+                    // var accountNum = line.Skip(4).Take(14);
+                    // var account = string.Join("", accountNum);
+                    //  aNums.Add(account);
+                    //I dont want to do it by the number of spaces because names can be different lengths 
+                    //and i didnt want my code to only work for this file
+                    var account = "";
+                   
+                    if (string.Join("", recAndAcc.Take(1)).Contains(" "))
+                    {
+                        account = string.Join("", recAndAcc.Skip(2).Take(10));
+                        accountNumbers.Add(account);
+                    }
+                    else { 
+                    account = string.Join("", recAndAcc.Skip(4).Take(10));
+                    accountNumbers.Add(account);
+                }
+                    /*var first = idk[1];
+                   var val = string.Join("", first);
+                   fName.Add(val);
+                   */
+
+                    //var num =idk.TakeLast(7);
+
+                    //var reversed = idk[idk.Length - 1].Reverse();
+                    
+
+                    var endOfLine = strArr[strArr.Length - 1];
+                   // noname.TakeLast()
+
+
+                    var amount = "";
+                    if (string.Join("", endOfLine.TakeLast(1)).Contains("."))
+                    {
+                        amount = string.Join("", endOfLine.TakeLast(5));
+                       // Console.WriteLine(string.Join("", endOfLine.TakeLast(5).Skip(4)));
+                       // Console.WriteLine(string.Join("", endOfLine.TakeLast(5)));
+                        amountList.Add(amount);
+                    }
+                    else
+                    {
+                        amount = string.Join("", endOfLine.TakeLast(7));
+                        amountList.Add(amount);
+                    }
+                    
+
+                    
+                    //Convert.ToDouble(num);
+                   // Console.WriteLine(num);
+                    
+
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error :{0} ", ex.Message);
+            }
+            for (int i = 0; i < recordNumbers.Count(); i++)
+            {
+                var value = 0.0;
+                if (validate.RecordNumber_IsValid(recordNumbers[i]) == false)
+                {
+                    Console.WriteLine("Invalid Record Number " +
+                        "on line " + $"{i + 1}");
+                }
+                if (validate.AccountNumber_IsValid(accountNumbers[i]) == false)
+                {
+                   
+                    Console.WriteLine(recordNumbers[i] + " Account Number " + $"{accountNumbers[i]}");
+                
+                }
+                try
+                {
+                     value = Convert.ToDouble(amountList[i]);
+                }catch
+                {
+                    
+                }
+                if (validate.Amount_IsValid(value) == false)
+                {
+
+                    Console.WriteLine(recordNumbers[i] + " Amount " + $"{amountList[i]}");
+                }
+                
+
+            }
+           
+        }
+
+           
+
+
+        }
+
+    }
+
---
+++ MyRecordProject/Validate.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyRecordProject
+{
+    class Validate
+    {
+        public bool RecordNumber_IsValid(string record)
+        {
+            if (String.IsNullOrWhiteSpace(record) || record.Any(x => Char.IsWhiteSpace(x)))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+
+
+        }
+        public bool AccountNumber_IsValid(string record)
+        {
+            if (record.Contains("-"))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+            
+
+        }
+        public bool FirstName_IsValid(string name)
+        {
+            
+            if (name != null && name.Length <=20)
+            {
+                return true;
+            }
+
+            throw new NotImplementedException();
+
+        }
+        public bool LastName_IsValid(string name )
+        {
+            if (name != null && name.Length <= 20)
+            {
+                return true;
+            }
+
+            throw new NotImplementedException();
+
+        }
+        public bool AddressLine1_IsValid(string address)
+        {
+            if (address != null && address.Length <= 30)
+            {
+                return true;
+            }
+
+            throw new NotImplementedException();
+
+        }
+        public bool City_IsValid(string city)
+        {
+            if (city != null && city.Length <= 20)
+            {
+                return true;
+            }
+
+            throw new NotImplementedException();
+
+        }
+        public bool State_IsValid(string state)
+        {
+            if (state != null && state.Length <= 2)
+            {
+                return true;
+            }
+
+            throw new NotImplementedException();
+
+        }
+        public bool Amount_IsValid(double amount)
+        {
+            if (amount != 0  && amount <= 10000 && amount.ToString().Length == 6)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+           
+
+        }
+    }
+}
